a73cc06468c801b0f3abb38cd7debd3f1260d50e
earth418 <alihafez42@gmail.com>
2025-07-29 14:44:32 -0400

    transitions finally

--- piechart.js
+++ piechart.js
@@ -16,9 +16,18 @@ const svg = d3.select("#export")
         .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
 
 var data = [
-    {"ICE": 4, "CBP": 5, "D":3},
-    {"ICE": 5, "CBP": 7, "D":40}
+    {"ICE": 4.1, "CBP": 5.4, "D":4},
+    {"ICE": 5.2, "CBP": 7.2, "D":3},
+    {"ICE": 7.2, "CBP": 8.1, "D":3},
+    {"ICE": 7.5, "CBP": 8.8, "D":4},
+    {"ICE": 8.1, "CBP": 10.5, "D":3.5},
 ];
+
+data = data.map(w => {
+    w["C"] = 1;
+    w["G"] = 2;
+    return w;
+});
 
 var arc = d3.arc()
     .innerRadius(0)
@@ -41,7 +50,7 @@ const IIC_arr = ["ICE","CBP"];
 function create_pie(data) {
     const total = data.reduce((a, b) => a + b[1], 0);
     const total_IIC = data.reduce((a, b) => a + (IIC_arr.includes(b[0])) * b[1], 0);
-    console.log(total_IIC);
+    console.log(data);
     let last_end = 0;
     const arr_angles = data.map((a, i) => {
         const ang = 2 * Math.PI * a[1] / total;
@@ -68,14 +77,14 @@ function create(data) {
         .enter()
         .append('path')
         .merge(u)
-        .transition(1000)
-        .attrTween("d", arcTween)
         .attr("d", d3.arc()
             .innerRadius(0)
             .outerRadius(radius)
             // .startAngle(d => -get_half_angle(d))
             // .endAngle(d => get_half_angle(d))
         )
+        // .transition(1000)
+        // .attrTween("d", arcTween)
         .attr("id", d => d.name)
         .attr("fill", d => (IIC_arr.includes(d.name)) ? "#dad" : "#ded")
         .attr("stroke", "black")
@@ -85,16 +94,32 @@ function create(data) {
     u
         .exit()
         .remove()
+    // return u;
 }
 
 let i = 0;
 let u = create(Object.entries(data[0]));
+const fp_data = 15;
 
 document.addEventListener("keydown", (e) => {
     if (e.key == "d") {
         i++;
-        let data_i = Object.entries(data[0]).map((d) => [d[0], d[1] + (i / 40.0) * (data[1][d[0]] - d[1])]);
+        let data_index = Math.floor(i / fp_data);
+        if (data_index == data.length)
+            return;
+
+        // Object.entries(data[data_index]).map((d, ind) => console.log(data[data_index+1][d[0]] - d[1]));
+        let data_i = Object.entries(data[data_index]).map((d, ind) => [d[0], d[1] + ((i % fp_data) / fp_data) * (data[data_index+1][d[0]] - d[1])]);
+        // console.log(data_i);
+        
+        // let data_i = create_pie(Object.entries(data[i]));
         create(data_i);
+        // u
+        // .data(data_i)
+        // .enter()
+        // .transition(1000)
+        // .attrTween("d", arcTween)
+            // .join("d")
     }
 });
 
